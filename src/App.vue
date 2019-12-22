fb27fdff608a1f5cfebcf3205d6ad75b3a5a51e5
JackWilb97 <jackwilburn@tutanota.com>
2019-12-21 22:13:47 -0800

    Initial commit with a landing page that needs styling

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <Landing/>
+  </div>
+</template>
+
+<script>
+import Landing from './components/Landing.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Landing
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+
+html {
+  background-image: linear-gradient(120deg, #84fab0 0%, #8fd3f4 100%);
+}
+</style>
---
+++ src/components/Landing.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="landing">
+    <h1>Jack Wilburn</h1>
+    <p>Full Stack Developer, Linux Enthusiast, Web Designer</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Landing',
+  props: {
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.landing {
+  text-align: center;
+  vertical-align: middle;
+  height: 100vh;
+  width: 100vw;
+}
+
+h1 {
+  font-size: 8em;
+}
+
+p {
+  font-size: 1em;
+}
+</style>
