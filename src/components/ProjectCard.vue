180bd6e93e24faf2649af9fb22964d0135fa264f
JackWilb97 <jackwilburn@tutanota.com>
2020-12-13 21:59:43 -0700

    Refactor components with tailwind UI, apply linting fixes

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -1,10 +1,52 @@
 <template>
-  <div class="card">
-    <div class="text">
-      <h3>{{ title }}</h3>
-      <p>{{ description }}</p>
-      <a v-if=github :href=github><img src="../assets/GitHub-Mark-120px-plus.png"></a>
-      <a v-if=link :href=link><img src="../assets/_ionicons_svg_ios-link.svg"></a>
+  <div class="mt-4 p-4 shadow-lg border-2 border-gray-50 rounded-none md:max-w-sm md:mx-auto">
+    <div class="font-bold text-xl mb-2">
+      {{ title }}
+    </div>
+    <p class="text-gray-500 text-base">
+      {{ description }}
+    </p>
+    <div class="mt-6 grid grid-cols-4 place-items-center">
+      <a />
+      <a
+        :href="github"
+        :class="muted(github)"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+          class="w-8"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M10 20l4-16m4 4l4 4-4 4M6 16l-4-4 4-4"
+          />
+        </svg>
+      </a>
+      <a
+        :href="link"
+        :class="muted(link)"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+          class="w-8"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M13.828 10.172a4 4 0 00-5.656 0l-4 4a4 4 0 105.656 5.656l1.102-1.101m-.758-4.899a4 4 0 005.656 0l4-4a4 4 0 00-5.656-5.656l-1.1 1.1"
+          />
+        </svg>
+      </a>
+      <a />
     </div>
   </div>
 </template>
@@ -12,48 +54,33 @@
 <script>
 export default {
   name: 'ProjectCard',
+
   props: {
-    title: String,
-    description: String,
-    github: String,
-    link: String
-  }
-}
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    github: {
+      type: String,
+      default: null,
+    },
+    link: {
+      type: String,
+      default: null,
+    },
+  },
+
+  methods: {
+    muted(value) {
+      return value === null ? 'text-gray-200' : '';
+    },
+  },
+};
 </script>
 
 <style scoped>
-.card {
-  display:inline-grid;
-  margin: 2%;
-  margin-top: 50px;
-  margin-bottom: 50px;
-  width: 25%;
-  background-color: white;
-  border-radius: 15px;
-  text-align: center;
-  min-width: 300px;
-}
-
-h3 {
-  margin: 0;
-  margin-top: 10%;
-  font-size: 2em;
-}
-
-p {
-  margin: 0;
-  padding: 5%;
-  font-size: 1em;
-}
-
-a {
-  display: inline;
-  margin-left: 10px;
-  margin-right: 10px;
-}
-
-img {
-  width: 30px;
-  height: 30px;
-}
 </style>
--- src/components/Resume.vue
+++ src/components/Resume.vue
@@ -1,76 +1,172 @@
 <template>
-  <div class="resume">
-    <div class="left">
-      <h2>Jack Wilburn</h2>
-      <img id="headshot" src="../assets/JackWilburnHeadshot.jpg" alt="Headshot">
-    </div>
-    <div class="right">
-      <!-- Jobs -->
-      <div class="resume-card">
-        <h2>Professional Experience</h2>
-        <h3>Software Developer</h3>
-        <h4>Scientific Computing and Imaging Insitute, University of Utah, Salt Lake City</h4>
-        <ul>
-            <li>Data mining, machine learning and web stack development with graph databases.
-            <li>Work with faculty and students in designing and developing client-server applications in support of research projects.
-            <li>Mentor students on software development best practices such as testing, CI/CD, and coding habits.
-            <li>Independently develop user interfaces, APIs, and server components with state-of-the-art web frontend technologies like HTML5, SCSS, Vue, React, Typescript, D3.js, Vega, chart.js and back end technologies such as Flask, ArangoDB, PostgreSQL, MongoDB, Redis, and Elasticsearch.
-            <li>Create and maintain build, testing, and deployment systems with Git, Github, Slack, Docker, AWS, Circle-CI, webpack, npm and many others.
-            <li>Develop high quality and well documented code.
-            <li>Aid in the specification of software requirements in coordination with faculty or team leads. Be responsible for a software package. Interface with collaboration partners. Mentor other Software Engineers.</li>
-        </ul>
-      
-        <h3>Data Scientist</h3>
-        <h4>Emperitas, Salt Lake City</h4>
-        <ul>
-            <li>Productionize nosql OrientDB server with 10+ million Ethereum transactions and 20000+ known users as an example of KYC/AML approach for Blockchain Training Alliance and FreeRan.ge bank.
-            <li>Build, deploy, and maintain machine learning models for Veterans First bank to predict loan outcomes.
-            <li>Build, deploy, and maintain CLV/ Journey Map models for clients and update their databases with pertinent information.
-            <li>Scrape publicly available data using rvest (R) and beautiful soup (Python) to develop strategies for clients and internal processes.
-            <li>Interface with APIs to reduce internal costs.
-            <li>Implemented web scraper to download 2,800+ crypto white papers for a 90x speed improvement.</li>
-        </ul>
-      </div>
+  <div id="resume">
+    <h2 class="mt-10 text-3xl text-green-500">
+      Resume
+    </h2>
 
-       <!-- Education -->
-      <div class="resume-card">
-        <h2>Education</h2>
-        <h3>Bachelor of Science: Mathematics</h3>
-        <h4>Westminster College, Salt Lake City</h4>
-        <ul>
-            <li>Engaged in math, data, and computer science classes to develop data manipulation, data visualization and coding skills. NCAA D2 soccer player.</li>
-        </ul>
-      </div>
+    <!-- Jobs -->
+    <div class="text-left p-4 max-w-4xl mx-auto mb-48">
+      <h3 class="mt-4 text-xl font-bold">
+        Professional Experience
+      </h3>
+      <h4 class="mt-2 text-lg">
+        Software Developer
+      </h4>
+      <p class="text-sm">
+        Scientific Computing and Imaging Insitute, University of Utah, Salt Lake City
+      </p>
+      <p class="mb-2 text-xs text-gray-500">
+        Aug 2019 - Present
+      </p>
+      <ul class="ml-8 list-disc text-base">
+        <li>
+          Lead software development efforts on over $3,000,000 of grant-funded research.
+        </li>
+        <li>
+          Work with faculty and students in designing and developing client-server applications.
+        </li>
+        <li>
+          Mentor students on software development best practices such as testing, CI/CD, and coding habits.
+        </li>
+        <li>
+          Independently develop user interfaces, APIs, and server components using technologies like HTML5, Vue.js, Typescript, D3.js, Flask, ArangoDB, Git, GitHub, Docker, AWS, GitHub Actions, webpack, npm, bash, and others.
+        </li>
+      </ul>
+
+      <h4 class="mt-6 text-lg">
+        Lead Data Scientist
+      </h4>
+      <p class="text-sm">
+        Emperitas, Salt Lake City
+      </p>
+      <p class="mb-2 text-xs text-gray-500">
+        May 2018 - Aug 2019
+      </p>
+      <ul class="ml-8 list-disc">
+        <li>
+          Productionized nosql OrientDB server with 10+ million Ethereum transactions and 20000+ known users as an example of KYC/AML approach.
+        </li>
+        <li>
+          Built, deployed, and maintained machine learning models for national bank to predict loan outcomes.
+        </li>
+        <li>
+          Built, deployed, and maintained CLV/ Journey Map models for clients and update their databases with pertinent information.
+        </li>
+        <li>
+          Scraped publicly available data using rvest (R) and beautiful soup (Python) to develop strategies for clients and internal processes.
+        </li>
+      </ul>
+
+      <!-- Education -->
+      <h2 class="mt-4 text-xl font-bold">
+        Education
+      </h2>
+      <h4 class="mt-2 text-lg">
+        Bachelor of Science: Mathematics
+      </h4>
+      <p class="text-sm">
+        Westminster College, Salt Lake City
+      </p>
+      <p class="mb-2 text-xs text-gray-500">
+        Aug 2015 - Dec 2018
+      </p>
+      <ul class="ml-8 list-disc">
+        <li>Engaged in math, data, and computer science classes to develop data manipulation, data visualization and coding skills. NCAA D2 soccer player.</li>
+      </ul>
 
       <!-- Articles -->
-      <div class="resume-card">
-        <h2>Articles</h2>
-        <h3>TODO: REGISTRY PAPER TITLE</h3>
-        <h4>TODO: JOURNAL NAME</h4>
-        <ul>
-            <li>Productionized website design that allowed for the storage and retrieval of deep brain stimulation (DBS) data.
-            <li>Co-wrote and edited the final manuscript.</li>
-        </ul>
-      </div>
+      <h2 class="mt-4 text-xl font-bold">
+        Articles
+      </h2>
+
+      <h4 class="mt-2 text-lg">
+        <a
+          href="http://sci.utah.edu/~vdl/papers/2020_vahc_sanguine.pdf"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            class="inline align-middle pb-1 w-4 text-green-500"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M13.828 10.172a4 4 0 00-5.656 0l-4 4a4 4 0 105.656 5.656l1.102-1.101m-.758-4.899a4 4 0 005.656 0l4-4a4 4 0 00-5.656-5.656l-1.1 1.1"
+            />
+          </svg>
+        </a>
+        Sanguine: Visual Analysis for Patient Blood Management
+      </h4>
+      <p class="text-sm">
+        Visual Analytics In Healthcare
+      </p>
+      <p class="mb-2 text-xs text-gray-500">
+        2020
+      </p>
+      <ul class="ml-8 list-disc">
+        <li>
+          Developed API to support patient blood management dashboard.
+        </li>
+        <li>
+          Co-wrote and edited the final manuscript.
+        </li>
+      </ul>
+
+      <h4 class="mt-6 text-lg">
+        <a
+          href="https://www.frontiersin.org/articles/10.3389/fninf.2020.00036/full"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            class="inline align-middle pb-1 w-4 text-green-500"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M13.828 10.172a4 4 0 00-5.656 0l-4 4a4 4 0 105.656 5.656l1.102-1.101m-.758-4.899a4 4 0 005.656 0l4-4a4 4 0 00-5.656-5.656l-1.1 1.1"
+            />
+          </svg>
+        </a>
+        The International Neuromodulation Registry: An Informatics Framework Supporting Cohort Discovery and Analysis
+      </h4>
+      <p class="text-sm">
+        Frontiers in Neuroinformatics
+      </p>
+      <p class="mb-2 text-xs text-gray-500">
+        2020
+      </p>
+      <ul class="ml-8 list-disc">
+        <li>
+          Productionized website design that allowed for the storage and retrieval of deep brain stimulation (DBS) data.
+        </li>
+        <li>
+          Co-wrote and edited the final manuscript.
+        </li>
+      </ul>
 
       <!-- Presentations -->
-      <div class="resume-card">
-        <h2>Presentations</h2>
-        <h3>How To De-anonymize 20,000 Ethereum Users, The Easy Way</h3>
-        <h4>Big Mountain Data and Dev, Eccles Theater, Salt Lake City</h4>
-        <ul>
-            <li>Used data from public sources to tie user account information to the anonymous blockchain as a proof of concept</li>
-        </ul>
-      </div>
-
-      <!-- Miscellaneous -->
-      <!-- <h2>Miscellaneous</h2>
-      <h3>Software Developer</h3>
-      <h4>Scientific Computing and Imaging Insitute, University of Utah, Salt Lake City</h4>
-      <ul>
-          <li>Responsibilities</li>
-      </ul> -->
-
+      <h2 class="mt-4 text-xl font-bold">
+        Presentations
+      </h2>
+      <h4 class="mt-2 text-lg">
+        How To De-anonymize 20,000 Ethereum Users, The Easy Way
+      </h4>
+      <p class="text-sm">
+        Big Mountain Data and Dev, Salt Lake City
+      </p>
+      <p class="mb-2 text-xs text-gray-500">
+        2019
+      </p>
+      <ul class="ml-8 list-disc">
+        <li>Used data from public sources to tie user account information to the anonymous blockchain as a KYC proof of concept</li>
+      </ul>
     </div>
   </div>
 </template>
@@ -78,76 +174,8 @@
 <script>
 export default {
   name: 'Resume',
-  props: {
-  }
-}
+};
 </script>
 
 <style scoped>
-.resume {
-  display:table;
-  text-align: center;
-  height: 100vh;
-  width: 100%;
-}
-
-.left {
-  text-align: center;
-  width: 40%;
-  display:inline-grid;
-  border-radius: 15px;
-  text-align: center;
-  min-width: 300px;
-}
-
-.right {
-  text-align: left;
-  width: 40%;
-  display:inline-grid;
-  margin-top: 50px;
-  margin-bottom: 50px;
-  border-radius: 15px;
-  min-width: 300px;
-}
-
-#headshot {
-  margin-left: 20%;
-  padding: 0;
-  width: 60%;
-  border-radius: 400px;
-}
-
-h1 {
-  margin: 0;
-  margin-bottom: 50px;
-  font-size: 3em;
-}
-
-.resume-card {
-  background-color: white;
-  border-radius: 15px;
-  margin: 10px;
-  padding: 15px;
-}
-
-h2 {
-  margin-top: 0;
-}
-
-h3 {
-  margin-bottom: 0;
-}
-
-h4 {
-  margin-top: 0;
-  margin-bottom: 0.5em;
-}
-
-p {
-  font-size: 1.5em;
-}
-
-ul, li {
-  margin-top: 0px;
-}
 </style>
