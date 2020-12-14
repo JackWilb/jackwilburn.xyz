890c45a3650bc65ddfeecd2a2d6728d70c4c19a7
JackWilb97 <jackwilburn@tutanota.com>
2020-12-13 21:52:32 -0700

    Add linting functionality

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: [
+    'plugin:vue/recommended',
+    '@vue/airbnb',
+  ],
+  parserOptions: {
+    ecmaVersion: 2020,
+    ecmaFeatures: {
+      modules: true,
+    },
+  },
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'max-len': ['off'],
+  },
+};
