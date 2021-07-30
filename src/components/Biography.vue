0ffc40bff0f10fa775200ef1c601721c3d2e9151
JackWilb <jackwilburn@tutanota.com>
2021-07-30 12:52:49 -0600

    Use webp for image

--- src/components/Biography.vue
+++ src/components/Biography.vue
@@ -201,7 +201,7 @@
     <div class="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2 lg:h-screen">
       <img
         class="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full"
-        src="../assets/dunes-edit-upscale.png"
+        src="../assets/dunes-edit-upscale.webp"
         alt=""
       >
     </div>
